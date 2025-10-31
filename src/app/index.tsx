7384b2929792328075cc40ae240d53adc14f780a
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-31 12:23:56 +0530

    main : few update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Text} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const modal = () => {
   return (
-<SafeAreaView className='flex-1 items-center justify-center'>
-  <Text className='text-5xl text-red-500'>Hey</Text>
-</SafeAreaView>
-  )
-}
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-red-500">Hey</Text>
+    </SafeAreaView>
+  );
+};
 
-export default modal
+export default modal;
