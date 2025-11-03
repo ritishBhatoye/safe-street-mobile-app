0560a8ab61ca73991d562ec54a180f517eea76f7
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-03 21:32:32 +0530

    TASK-9 : glassEffect add to /(tabs)/_layout.tsx

--- src/components/atoms/GlassTabBarBackground.tsx
+++ src/components/atoms/GlassTabBarBackground.tsx
@@ -1,12 +1,31 @@
 import { BlurView } from "expo-blur";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const GlassTabBarBackground = () => (
-  <BlurView
-    intensity={40}
-    tint="systemUltraThinMaterialLight"
-    style={StyleSheet.absoluteFill}
-  />
+  <View style={StyleSheet.absoluteFill}>
+    <BlurView
+      intensity={10}
+      tint="light"
+      style={[
+        StyleSheet.absoluteFill,
+        {
+          borderRadius: 30,
+          overflow: "hidden",
+        },
+      ]}
+    />
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        {
+          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          borderRadius: 30,
+          borderWidth: 1,
+          borderColor: "rgba(255, 255, 255, 0.5)",
+        },
+      ]}
+    />
+  </View>
 );
 
 export default GlassTabBarBackground;
