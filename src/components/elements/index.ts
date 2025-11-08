6cdfc45635a4824d69c671820bc570c04b24fb87
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-11-08 12:39:25 +0530

    TASK-12 : major improvement

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -2,8 +2,6 @@ export { CategoryChip } from './CategoryChip';
 export { EmergencyContactCard } from './EmergencyContactCard';
 export { IncidentCard } from './IncidentCard';
 export { MacroStat } from './MacroStat';
-
 export { QuickActionButton } from './QuickActionButton';
 export { SafetyScoreCard } from './SafetyScoreCard';
-export { SearchBar } from './SearchBar';
 export { StatCard } from './StatCard';
