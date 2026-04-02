14f02c06ead37928fb6ac2628209ceef400a6316
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-04-02 22:37:16 +0530

    chore: src/components/forms/OtpForm/EmailInput.tsx

--- src/components/forms/OtpForm/EmailInput.tsx
+++ src/components/forms/OtpForm/EmailInput.tsx
@@ -1,9 +1,16 @@
+import { emailSchema } from "@/utils/validations/authValidation";
 import { FormikProps, useFormik } from "formik";
 import React from "react";
+import { Keyboard } from "react-native";
 
 const EmailInput = ({ loading, onSubmit, initialValues }: EmailFormProps) => {
-  const formik: FormikProps<EmailTypes> = useFormik<EmailTypes>({
-    initialValues: initialValues ?? { email: "" },
+  const formik: FormikProps<EmailTypes> = useFormik<EmailFormProps>({
+    initialValues: initialValues || { email: "" },
+    validationSchema: emailSchema,
+    onSubmit: (values) => {
+      Keyboard.dismiss();
+      onSubmit(values);
+    },
   });
   return <div>EmailInput</div>;
 };
