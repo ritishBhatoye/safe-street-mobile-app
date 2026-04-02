0651f465be5ea46372d1d57377c6fa098abe2536
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-04-02 23:11:32 +0530

    chore: src/components/forms/OtpForm/PasswordInput.tsx

--- src/components/forms/OtpForm/PasswordInput.tsx
+++ src/components/forms/OtpForm/PasswordInput.tsx
@@ -1,11 +1,16 @@
 import { resetPasswordSchema } from "@/utils/validations/authValidation";
 import { FormikProps, useFormik } from "formik";
 import React from "react";
+import { Keyboard } from "react-native";
 
 const PasswordInput = ({ loading, initialValues, onSubmit }: ResetPasswordFormProps) => {
   const formik: FormikProps<ResetPasswordTypes> = useFormik({
     validationSchema: resetPasswordSchema,
-    initialValues: initialValues ?? { password: "" },
+    initialValues: initialValues || { password: "", confirmPassword: "" },
+    onSubmit: (values) => {
+      Keyboard.dismiss();
+      onSubmit(values);
+    },
   });
   return <View></View>;
 };
